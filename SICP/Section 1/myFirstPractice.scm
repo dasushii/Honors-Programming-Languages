fdb89a70dafa102ee6079ce8fbe3842d7ad33c8c
Eric Wang <ericwang@westminster.net>
2021-01-14 13:59:23 -0500

    Created myFirstPractice
    ye

---
+++ SICP/Section 1/myFirstPractice.scm
@@ -0,0 +1,9 @@
+(define (main)
+    (println "Author: Eric Wang ericwang@westminster.net")
+    )
+; Problem 1
+(println "Phone number: 1 (678) 910-1112")
+(inspect (* 1 1))
+(inspect (+ 600 70 8))
+(inspect (/ 910 1))
+(inspect (- 2000 888))
